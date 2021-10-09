1f8d1e9c4a902c665792e05c73363592e5c6e7e1
MinhKhangTran <minhkhang.tran@gmx.de>
2021-10-09 08:05:15 +0200

    :tada: start new project

---
+++ src/components/Layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/main.scss";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default Layout;
