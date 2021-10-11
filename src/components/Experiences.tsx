bcc0c2a87b304e676fa83870b174c424a17e9ce3
MinhKhangTran <minhkhang.tran@gmx.de>
2021-10-11 04:55:19 +0200

    :sparkles: education

--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Badge, Container } from "react-bootstrap";
 
 const Experiences = () => {
   const expArray = [
@@ -9,7 +9,7 @@ const Experiences = () => {
       date: "December 2015 - Present",
       company: "netflix",
       color: "danger",
-      desc: "Thundercats cray crucifix, williamsburg migas gastropub hexagon jean shorts kombucha sustainable coloring book.",
+      desc: "Thundercats cray crucifix, williamsburg migas gastropub hexagon jean shorts kombucha sustainable coloring book jean shorts kombucha sustainable coloring book.",
     },
     {
       id: 2,
@@ -17,7 +17,7 @@ const Experiences = () => {
       date: "July 2013 - December 2015",
       company: "samsung",
       color: "info",
-      desc: "Air plant cliche tilde jean shorts franzen gentrify chillwave. Hella dreamcatcher +1 jianbing kickstarter beard. You probably haven't heard of them offal deep v green juice.",
+      desc: "Air plant cliche tilde jean shorts franzen gentrify chillwave. Hella dreamcatcher +1 jianbing kickstarter beard. You probably haven't heard of them offal deep v green juice You probably haven't heard of them offal deep v green juice.",
     },
     {
       id: 3,
@@ -25,13 +25,30 @@ const Experiences = () => {
       date: "August 2008 - July 2013",
       company: "apple",
       color: "secondary",
-      desc: "Cronut chambray mustache brooklyn shaman gastropub freegan letterpress, dreamcatcher plaid drinking vinegar.",
+      desc: "Cronut chambray mustache brooklyn shaman gastropub freegan letterpress, dreamcatcher plaid drinking vinegar dreamcatcher plaid drinking vinegar.",
     },
   ];
   return (
     <section id="experiences">
       <Container>
         <h2 className="fw-bold text-cyan-400 mt-5 mb-4">Experiences</h2>
+        {expArray.map((exp) => {
+          const { id, title, date, company, color, desc } = exp;
+          return (
+            <article className="my-4" key={id}>
+              <div className="exp_heading d-lg-flex justify-content-lg-between ">
+                <h3 className="exp_heading_head fs-1">{title}</h3>
+                <p className="text-cyan-700 fs-5">{date}</p>
+              </div>
+              <div
+                className={`exp_company bg-${color} bg-gradient d-inline-block py-1 px-2 rounded text-uppercase fw-bold text-white`}
+              >
+                <span className="exp_company_name">{company}</span>
+              </div>
+              <p className="lead mt-2 mb-4 exp_company_desc">{desc}</p>
+            </article>
+          );
+        })}
       </Container>
     </section>
   );
